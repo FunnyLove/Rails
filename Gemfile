8074dd1ecbee101d384a6f9a8775c4abc7f62791
Sandra Powell <funnyloveletters@hotmail.com>
2012-12-17 13:38:57 -0500

    Removed welcome tutorial and completed Skill Share project

--- Gemfile
+++ Gemfile
@@ -1,9 +1,11 @@
 source 'https://rubygems.org'
 
 gem 'rails', '3.2.1'
-
+gem 'bootstrap-sass'
+gem 'devise'
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
+
 
 gem 'sqlite3'
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Firstproject::Application.routes.draw do
-  get "welcome/index"
+  
+  devise_for :users
+
+  root :to => 'static_pages#home'
+
+  get "about" => "static_pages#about"
+  
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -50,7 +56,7 @@ Firstproject::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-   root :to => 'welcome#index'
+   
 
   # See how all your routes lay out with "rake routes"
 
